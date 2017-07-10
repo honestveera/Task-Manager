893c15bed2d659fdd6be8472869188d2fd1977e2
honestveera <honestraj.it@gmail.com>
2017-07-10 12:00:21 +0530

    Changed my production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Mailer.
-  config.action_mailer.default_url_options = { host: 'taskmanagement.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'honesttaskmanagement.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
